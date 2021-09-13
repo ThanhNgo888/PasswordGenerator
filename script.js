72b7178a7c21e63a49f64789d01a5c64c8dd61ac
ThanhNgo888 <thanhngo26@hotmail.com>
2021-09-13 09:51:34 -0700

    modified js

--- script.js
+++ script.js
@@ -8,20 +8,20 @@ function generate(){
 
     //when a user hits the generate button,this variable will get set
     let password = "";
-
+    //================================================================
     //create for loop to choose password characters
     for(var i = 0; i <=complexity; i++){
         //minus 1 because we started from 0
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
-
+    //================================================================
     //add password to textbox/display area
     document.getElementById("display").value = password;
 
     //add password to previously generated passwords section
     document.getElementById("lastPasswords").innerHTML += password + "<br />";
 }
-
+//================================================================
 //set default length display of 25
 document.getElementById("length").innerHTML = "Length: 25";
 
@@ -33,7 +33,7 @@ document.getElementById("slider").oninput = function(){
         document.getElementById("length").innerHTML = "Length: 1";
     }
 }
-
+//================================================================
 //function to copy password to clipboard
 function clipboardPW(){
     document.getElementById("display").select();
